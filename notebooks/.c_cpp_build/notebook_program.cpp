50066ec2da5bb86e50827f539fdd0b1da425f625
zxcv770880789@gmail.com <nthu106021271@gapp.nthu.edu.tw>
2026-09-15 20:26:40 +0800

    Update teaching materials 2026-09-15 20:26

--- notebooks/.c_cpp_build/notebook_program.cpp
+++ notebooks/.c_cpp_build/notebook_program.cpp
@@ -1,11 +1,18 @@
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int main() {
-    int values[] = {10, 20, 30};
+    vector<int> a = {10, 20, 30};
 
-    for (int value : values) {
-        std::cout << value << '\n';
+    a.push_back(40);
+    a.push_back(50);
+
+    cout << a.size() << endl;
+
+    for (int i = 0; i < a.size(); i++) {
+        cout << a[i] << " ";
     }
 
     return 0;
